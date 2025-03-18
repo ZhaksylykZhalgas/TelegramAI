f6a8c7f6754e4b28e08c6952ca8167cbfc791bfe
ZhaksylykZhalgas <ziqzaq2005@gmail.com>
2025-03-19 01:52:20 +0500

    add bot and Configuration(Finally)

--- main/main_bot.py
+++ main/main_bot.py
@@ -5,7 +5,7 @@ bot = AsyncTeleBot(settings.TOKEN_BOT, parse_mode='HTML')
 
 @bot.message_handler(commands=['start', 'help'])
 async def send_welcome(message):
-    await bot.send_message(message.chat.id, """\n Hi my name is <b>Zhalgas</b>, I am learning to create AI bot on telegram.""")
+    await bot.send_message(message.chat.id, """\n Hi my name is Zhalgas, I am learning to create AI bot on telegram.""")
 
 @bot.message_handler(func=lambda message: True)
 async def echo_message(message):
